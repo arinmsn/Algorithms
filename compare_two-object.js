be83ebdf416d84d46c65fe178f1b75ec66463559
Arin M <arinmsn@gmail.com>
2019-08-23 03:56:14 -0700

    Comparing two objects (arrays too)

---
+++ compare_two-object.js
@@ -0,0 +1,30 @@
+/*
+    const user1 = {
+        name: 'Tom',
+        age: 40,
+    };
+
+    const user2 = {
+        name: 'Tom',
+        age: 40,
+    };
+
+    console.log(user1 == user2);
+    console.log(user1 === user2);
+*/
+
+/*
+    Solution:
+    false
+    false
+
+    Objects are never equal to each other.
+
+    If we were to say, const user2 = user1;
+
+    true
+    true
+
+    console.log(JSON.stringify(user1) === JSON.stringify(user2)); // true 
+    console.log([] == []); // false
+*/
