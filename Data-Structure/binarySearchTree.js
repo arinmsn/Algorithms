da946f6fb2a97d8d01a9bd450760ca7237a2c8fe
arinmsn <arinmsn@gmail.com>
2019-08-26 08:14:15 -0700

    Binary Search Tree - Insert method

--- Data-Structure/binarySearchTree.js
+++ Data-Structure/binarySearchTree.js
@@ -40,3 +40,37 @@ function factorial(num){
         return num * factorial(num - 1);
     }
 }
+
+// Call Stack
+// It represents what order our F(n) are called in and 
+// what variables are called with
+
+factorial(4);
+/* 
+We use recursion to get to the base case. Then we unwind.
+num = 1     factorial(1)    base case
+num = 2     return 2 * factorial(1)
+num = 3     return 3 * factorial(2)
+num = 4     return 4 * factorial(3)
+*/
+
+// Insert method
+function BST(value){
+    this.value = value; 
+    this.left = null; 
+    this.right = null;
+}
+
+// Just crated one node with no children
+var bst = new BST(50); 
+
+BST.prototype.insert = function(value){
+    // Is value < or > than root node?
+    if (value <= this.value) {
+        // if there is no left child
+        if (!this.left) this.left = new BST(value);
+        else this.left.insert(value);
+    } else if (value > this.value) {
+
+    }
+};
