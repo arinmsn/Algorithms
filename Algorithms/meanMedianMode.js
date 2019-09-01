c72555c498a82bef654d4484144ad97bfa71d633
arinmsn <arinmsn@gmail.com>
2019-08-31 20:23:30 -0700

    Main F(n) done; Mean F(n) done.

---
+++ Algorithms/meanMedianMode.js
@@ -0,0 +1,54 @@
+/*
+    #Functional Programming
+
+    Three properties: Mean, Mean, Mode
+    Four separate functions
+
+    function getMean(array){
+        // Your code
+    }
+
+    function getMedian(array){
+        // Your code
+    }
+
+    function getMode(array){
+        // Your code
+    }
+
+    function meanMedianMode(array){
+        // Call other 3 functions
+        // Return object which has mean, median, and mode
+    }
+
+    The "mode" is the number that is repeated most often.
+    The "mean" is the "average."
+    The "median" is the "middle" value in the list of numbers.
+*/
+
+function getMean(array) {
+  var sum = 0;
+
+  array.forEach(num => {
+    sum += num;
+  });
+
+  var mean = sum / array.length;
+  return mean;
+}
+
+function getMedian(array) {
+  // Your code
+}
+
+function getMode(array) {
+  // Your code
+}
+
+function meanMedianMode(array) {
+  return {
+    mean: getMean(array),
+    median: getMedian(array),
+    mode: getMode(array)
+  };
+}
