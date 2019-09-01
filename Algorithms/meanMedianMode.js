663c6adc5dc7cce75781c42e2746f7b81533db46
arinmsn <arinmsn@gmail.com>
2019-08-31 20:59:01 -0700

    Algorithm for Mean-Mode-Median works

--- Algorithms/meanMedianMode.js
+++ Algorithms/meanMedianMode.js
@@ -43,10 +43,42 @@ function getMedian(array) {
     return a - b;
   });
   var median;
+
+  // Odd # of elements
+  if (array.length % 2 !== 0) {
+    median = array[Math.floor(array.length / 2)];
+  } else {
+    var mid1 = array[array.length / 2 - 1];
+    var mid2 = array[array.length / 2];
+    median = (mid1 + mid2) / 2;
+  }
+  return median;
 }
 
+// #s that repeat most often
 function getMode(array) {
-  // Your code
+  // Hash Table
+  var modeObj = {};
+
+  array.forEach(num => {
+    if (!modeObj[num]) modeObj[num] = 0;
+    modeObj[num]++;
+  });
+
+  var maxFrequency = 0;
+  var modes = [];
+
+  for (var num in modeObj) {
+    if (modeObj[num] > maxFrequency) {
+      modes = [num];
+      maxFrequency = modeObj[num];
+    }
+    // Case where current number has same frequency as maxFrequency
+    else if (modeObj[num] === maxFrequency) modes.push(num);
+  }
+  // No mode if every number happens with same frequency
+  if (modes.length === Object.keys(modeObj).length) modes = [];
+  return modes;
 }
 
 function meanMedianMode(array) {
@@ -56,3 +88,8 @@ function meanMedianMode(array) {
     mode: getMode(array)
   };
 }
+
+// Testing Algorithm
+array = [1, 2, 3, 4, 5, 4, 6, 1];
+meanMedianMode(array);
+// Works! { mean: 3.25, median: 3.5, mode: [ '1', '4' ] }
