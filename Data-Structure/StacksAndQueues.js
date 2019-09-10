2686886320f0d56aef7dce4d82950e9aa36597a8
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-09-10 15:44:23 -0700

    Create StacksAndQueues.js

---
+++ Data-Structure/StacksAndQueues.js
@@ -0,0 +1,25 @@
+ A stack is a collection of elements that supports two principle operations,
+ push and pop. Stacks are last-in, first-out data structures. In other words,
+ the last item pushed is the first one popped. So if we had a stack with one
+ item in it and we pushed another item onto the stack, followed by another,
+ we'd have a stack with three items in it.
+ 
+ Pushing an item onto the stack is constan-time operation
+ 
+ 
+ Queues work  a little differently.
+ 
+ Queue: collection that supports adding and removing
+ * First item added is the first item out
+ 
+ Practical Application
+ - Stacks
+  * Expression processing
+  * Backtracking: browser back button,
+  
+ - Queue
+  * Order processing
+  * Messaging system
+  
+ 
+  
