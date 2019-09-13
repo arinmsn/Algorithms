30efd0d753da9b4c659f9edc38d4788d56ec36a0
arinmsn <arinmsn@gmail.com>
2019-09-12 21:35:10 -0700

    Initial commit - some instructions

---
+++ Algorithms/MergeSort.js
@@ -0,0 +1,19 @@
+/*
+    Input: initial unsorted array
+
+    Array is split into several arrays are compared
+    against each other and result is merged into one array. 
+
+
+    function mergeSort(arr){
+        // Input is a single, unsorted array as a 
+        // parameter split the array into two halves
+    }
+
+    function merge (array1, array2) {
+        // Takes in two sorted arrays a parameters
+        // merges those sorted arrays into one sorted array
+        // returns one sorted array
+    }
+
+*/
