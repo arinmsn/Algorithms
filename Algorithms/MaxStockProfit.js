1e67ee307858a4c2271088378443722766e21d9d
arinmsn <arinmsn@gmail.com>
2019-09-13 21:43:09 -0700

    Instructions

---
+++ Algorithms/MaxStockProfit.js
@@ -0,0 +1,19 @@
+/*
+
+function maxStockProfit(pricesArr){
+    // Takes in an array of prcies for a stock
+    // as a parameter and results hte max possible
+    // profit of the day (we could have made)
+
+    // What is the best price to sell & best price to buy
+}
+
+Example: [ 40, 38, 36, 12, 15, 8, 10, 30, 60, 160]
+Maximum profit we could have made is $174.
+Since 8 is best price we could buy and 160 is best price to sell
+
+Notes: If no profit is possible for that day, return -1.  
+A max profit of 0 is treated as any other max profit value
+O(n) -linear- is the one we are aiming for
+
+*/
