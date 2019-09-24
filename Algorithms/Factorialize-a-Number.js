99c5be282eb8305d3a8b4bcaa12ca0b55c167120
arinmsn <arinmsn@gmail.com>
2019-09-23 20:43:22 -0700

    Two more working solutions (different)

--- Algorithms/Factorialize-a-Number.js
+++ Algorithms/Factorialize-a-Number.js
@@ -9,6 +9,23 @@ function factorialize(num) {
 }
 */
 
+// Solution 2
+/*
+function factorialize(num) {
+  if (num === 0) return 1;
+  return num * factorialize(num - 1);
+}
+*/
+
+// Solution 3
+
+function factorialize(num) {
+  for (var a = 1; num >= 1; num--) {
+    a *= num;
+  }
+  return a;
+}
+
 console.log(factorialize(0));
 console.log(factorialize(10));
 console.log(factorialize(20));
