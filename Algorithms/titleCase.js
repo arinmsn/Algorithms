dcd17bf25f821881c5fd62fea0f8573119b5d9a1
arinmsn <arinmsn@gmail.com>
2019-09-24 21:30:18 -0700

    lowercase title of a book is Capitalized.

---
+++ Algorithms/titleCase.js
@@ -0,0 +1,22 @@
+String.prototype.replaceAt = function(index, character) {
+  return (
+    this.substr(0, index) + character + this.substr(index + character.length)
+  );
+};
+
+function titleCase(str) {
+  var newTitle = str.split(" ");
+  var updatedTitle = [];
+  for (var st in newTitle) {
+    updatedTitle[st] = newTitle[st]
+      .toLowerCase()
+      .replaceAt(0, newTitle[st].charAt(0).toUpperCase());
+  }
+  return updatedTitle.join(" ");
+}
+
+// str = "the woman in white";
+str = "and then there were none";
+// str = "the name of the rose";
+// str = "the big sleep";
+console.log(titleCase(str));
