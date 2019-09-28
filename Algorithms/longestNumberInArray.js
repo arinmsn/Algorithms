ffc4a91c30cf20711a2630585817d172bfdbb65a
arinmsn <arinmsn@gmail.com>
2019-09-27 20:12:27 -0700

    Find largest # in sub-array: Solved

---
+++ Algorithms/longestNumberInArray.js
@@ -0,0 +1,26 @@
+// Write a function that returns largest number from
+// each sub-array
+
+arr = [
+  [10, 5, 7, 1],
+  [49, 13, 41, 43],
+  [55, 129, 80, 199],
+  [838, 610, 999, 750],
+  [4000, 2303, 1120, 3920]
+];
+
+function largestNumber(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+    results[n] = largestNumber;
+  }
+  return results;
+}
+
+console.log(largestNumber(arr)); // Should return [10, 49, 199, 999, 4000]
