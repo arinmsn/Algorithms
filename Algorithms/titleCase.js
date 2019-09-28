c679ab1f8795d8f8e1d14752943f7a1f33e415ad
arinmsn <arinmsn@gmail.com>
2019-09-27 19:46:30 -0700

    More solutions for titleCase (uppercase) problem

--- Algorithms/titleCase.js
+++ Algorithms/titleCase.js
@@ -4,6 +4,7 @@ String.prototype.replaceAt = function(index, character) {
   );
 };
 
+// Solution 1
 function titleCase(str) {
   var newTitle = str.split(" ");
   var updatedTitle = [];
@@ -20,3 +21,29 @@ str = "and then there were none";
 // str = "the name of the rose";
 // str = "the big sleep";
 console.log(titleCase(str));
+
+// Solution 2
+function titleCase(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function(va) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
+
+// Solution 3
+function titleCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+}
+
+// Solution 4
+function titleCase(str) {
+  str = str.split(" ");
+
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].toLowerCase().split("");
+    str[i][0] = str[i][0].toUpperCase();
+    str[i] = str[i].join("");
+  }
+  return str.join(" ");
+}
